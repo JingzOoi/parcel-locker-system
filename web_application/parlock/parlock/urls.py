d7c1657702496b6f0a04ee837a07bf811fa0e6cb
JZ <ooijingzhi@gmail.com>
2021-05-29 10:39:18 +0800

    fix admin name

--- web_application/parlock/parlock/urls.py
+++ web_application/parlock/parlock/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include("central.urls")),
     path('api/', include("api.urls")),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
 ]
