603c5981e84b757a430c6b41922399646018099d
JZ <ooijingzhi@gmail.com>
2021-06-20 13:36:00 +0800

    some endpoints

--- web_application/parlock/api/urls.py
+++ web_application/parlock/api/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("locker/<int:locker_base_id>/activity/query", views.locker_base_query_activity),
+    # path("locker/<int:locker_base_id>/activity/query", views.locker_base_query_activity),
     path("locker/<int:locker_base_id>/<int:locker_unit_id>/activity/add/<int:activity_type>", views.locker_add_activity),
+    path("locker/<int:locker_base_id>/parcel/query", views.parcel_query),
     # path("parcel/<int:parcel_id>/activity/add/<int:activity_type>", views.locker_add_activity),
 ]
--- web_application/parlock/api/views.py
+++ web_application/parlock/api/views.py
@@ -1,4 +1,6 @@
-from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+from django.http.response import Http404, HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from central.models import *
 
@@ -9,10 +11,10 @@ from central.models import *
 
 def locker_add_activity(request, locker_base_id: int, locker_unit_id: int, activity_type: int):
     lb = get_object_or_404(LockerBase, pk=locker_base_id)
-    lu = get_object_or_404(LockerUnit, pk=locker_unit_id) if locker_unit_id else None
-    la = LockerActivity(locker_base=lb, locker_unit=lu, _type=activity_type)
+    lu = get_object_or_404(LockerUnit, pk=locker_unit_id) if locker_unit_id != 0 else None
+    la = LockerActivity(locker_base=lb, locker_unit=lu, type=activity_type)
     la.save()
-    return HttpResponse(f"added activity {la._type} for locker {la.locker_base.name}/{la.locker_unit.name}.")
+    return HttpResponse(f"added activity {la.type} for locker {la.locker_base.name}")
 
 
 def locker_base_query_activity(request, locker_base_id):
@@ -22,3 +24,18 @@ def locker_base_query_activity(request, locker_base_id):
     for la in la_list:
         la_attr += f"<p>{la.datetime}, {la.locker_base}, {la.locker_unit}, {la.ActivityType(la._type).label}</p>"
     return HttpResponse(la_attr)
+
+
+def parcel_query(request, locker_base_id: int):
+    lb = get_object_or_404(LockerBase, pk=locker_base_id)
+    if request.method == "GET":
+        tn = request.GET["tn"]
+        parcel = get_object_or_404(Parcel, tracking_number=tn)
+        if parcel.destination_locker != lb:
+            raise Http404
+        else:
+            la = LockerActivity(locker_base=lb, locker_unit=None, type=5)
+            la.save()
+            pl = ParcelActivity(parcel=parcel, type=2, associated_locker_activity=la)
+            pl.save()
+            return JsonResponse({"parcel_activity": hash(pl)})
