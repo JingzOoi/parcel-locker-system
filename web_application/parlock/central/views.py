b2f839db55264c784fb4fdb36fa78fae43aa0dae
JZ <ooijingzhi@gmail.com>
2021-06-23 14:35:03 +0800

    home stats, location redirect register parcel, login required

--- web_application/parlock/central/views.py
+++ web_application/parlock/central/views.py
@@ -2,10 +2,11 @@ from django import http
 from django.http.response import HttpResponse
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import LockerBase, Parcel, ParcelActivity
+from .models import LockerActivity, LockerBase, Parcel, ParcelActivity
 from .forms import ParcelForm
 from datetime import datetime
 from django.contrib.postgres.search import SearchVector
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -20,7 +21,10 @@ def index(request):
 
 def home(request):
     context_dict = {}
-    return render(request, "home.html")
+    if request.user.is_authenticated and request.user.is_admin:
+        context_dict["latest_pa"] = ParcelActivity.objects.order_by("-datetime")[:10]
+        context_dict["latest_la"] = LockerActivity.objects.order_by("-datetime")[:10]
+    return render(request, "home.html", context=context_dict)
 
 # profile
 
@@ -58,6 +62,7 @@ def parcel_details(request, parcel_id):
     return render(request, "parcel/details.html", context=context_dict)
 
 
+@login_required(login_url="login")
 def add_parcel_action(request):
     context_dict = {}
     if request.method == "POST":
@@ -70,6 +75,12 @@ def add_parcel_action(request):
             return http.HttpResponseRedirect("parcel/")
         else:
             context_dict["form"] = f
+    elif request.method == "GET" and "locker" in request.GET:
+        try:
+            lb = LockerBase.objects.get(pk=request.GET["locker"])
+            context_dict["form"] = ParcelForm({"destination_locker": lb})
+        except LockerBase.DoesNotExist:
+            context_dict["form"] = ParcelForm()
     else:
         context_dict["form"] = ParcelForm()
     return render(request, "parcel/register.html", context=context_dict)
