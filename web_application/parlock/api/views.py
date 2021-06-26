2c53d190dcb9606fe1a0b6fcc25c391f9012fdf5
JZ <ooijingzhi@gmail.com>
2021-06-26 18:34:34 +0800

    mostly formatting changes, plus require_post and change register json response

--- web_application/parlock/api/views.py
+++ web_application/parlock/api/views.py
@@ -1,21 +1,24 @@
 from django.http.request import HttpRequest
-from django.http.response import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseNotModified
+from django.http.response import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseNotModified
 from django.http import JsonResponse
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from central.models import *
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 import logging
 
 # Create your views here.
 
-# Dev note: security comes later.
 
-
+@require_POST
 @csrf_exempt
 def locker_add_activity(request, locker_id: int, activity_type: str):
+    """
+    Concentrated endpoint for the locker base to query the webserver.
+    """
     try:
         lb = get_object_or_404(LockerBase, pk=locker_id)
-        if LockerBase.verify(request.POST["v_code"]) != lb:
+        if LockerBase.verify(request.POST["verification_code"]) != lb:
             return HttpResponseBadRequest()
         else:
             activity_type = activity_type.lower()
@@ -35,19 +38,25 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
                 # reports locker units connected to webserver.
                 lu = get_object_or_404(LockerUnit, pk=request.POST["unit_id"])
                 if lb.add_activity(activity_type=LockerActivity.ActivityType.REGISTER, locker_unit=lu):
-                    return JsonResponse({"success": True})
+
+                    return JsonResponse({
+                        "success": True,
+                        "length": lu.length,
+                        "width": lu.width,
+                        "height": lu.height
+                    })
                 else:
                     return HttpResponseNotModified()
             elif activity_type == "change":
                 # requests for a change in verification code.
                 la = lb.change_v_code()
                 if isinstance(la, LockerActivity):
-                    return JsonResponse({"success": True, "v_code": lb.verification_code})
+                    return JsonResponse({"success": True, "verification_code": lb.verification_code})
                 else:
                     return HttpResponseNotModified()
             elif activity_type == "parcel":
                 # queries the webserver if the parcel belongs to this lockerbase.
-                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tn"])
+                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tracking_number"])
                 pa = parcel.add_activity(locker_base=lb, activity_type=ParcelActivity.ActivityType.QUERY)
                 if pa and isinstance(pa, ParcelActivity):
                     return JsonResponse({"success": True})
@@ -55,7 +64,7 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
                     return HttpResponseForbidden()
             elif activity_type == "scandim":
                 # informs the webserver that the parcel just had its dimensions scanned.
-                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tn"])
+                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tracking_number"])
                 pa = parcel.add_activity(locker_base=lb, activity_type=ParcelActivity.ActivityType.CHECKIN)
                 if pa and isinstance(pa, ParcelActivity):
                     return JsonResponse({"success": True})
@@ -65,14 +74,22 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
                 # is a set of two
                 # UNLOCK - DEPOSIT_REQ if complete=false
                 # LOCK - DEPOSIT if complete=true
-                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tn"])
+                parcel = get_object_or_404(Parcel, tracking_number=request.POST["tracking_number"])
                 lu = get_object_or_404(LockerUnit, pk=request.POST["unit_id"])
                 if "complete" in request.POST:
                     is_complete = eval(request.POST["complete"])
                     if isinstance(is_complete, bool) and is_complete:
-                        pa = parcel.add_activity(locker_base=lb, locker_unit=lu, activity_type=ParcelActivity.ActivityType.DEPOSIT)
+                        pa = parcel.add_activity(
+                            locker_base=lb,
+                            locker_unit=lu,
+                            activity_type=ParcelActivity.ActivityType.DEPOSIT
+                        )
                     else:
-                        pa = parcel.add_activity(locker_base=lb, locker_unit=lu, activity_type=ParcelActivity.ActivityType.DEPOSITREQ)
+                        pa = parcel.add_activity(
+                            locker_base=lb,
+                            locker_unit=lu,
+                            activity_type=ParcelActivity.ActivityType.DEPOSITREQ
+                        )
                     if pa and isinstance(pa, ParcelActivity):
                         return JsonResponse({"success": True})
                     else:
@@ -87,9 +104,17 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
                 if "complete" in request.POST:
                     is_complete = eval(request.POST["complete"])
                     if isinstance(is_complete, bool) and is_complete:
-                        pa = parcel.add_activity(locker_base=lb, locker_unit=lu, activity_type=ParcelActivity.ActivityType.WITHDRAW)
+                        pa = parcel.add_activity(
+                            locker_base=lb,
+                            locker_unit=lu,
+                            activity_type=ParcelActivity.ActivityType.WITHDRAW
+                        )
                     else:
-                        pa = parcel.add_activity(locker_base=lb, locker_unit=lu, activity_type=ParcelActivity.ActivityType.WITHDRAWREQ)
+                        pa = parcel.add_activity(
+                            locker_base=lb,
+                            locker_unit=lu,
+                            activity_type=ParcelActivity.ActivityType.WITHDRAWREQ
+                        )
                     if pa and isinstance(pa, ParcelActivity):
                         return JsonResponse({"success": True})
                     else:
@@ -105,7 +130,10 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
                 elif p is False:
                     return JsonResponse({"success": False})
                 elif isinstance(p, Parcel):
-                    pa = p.add_activity(locker_base=lb, activity_type=ParcelActivity.ActivityType.WITHDRAWQR)
+                    pa = p.add_activity(
+                        locker_base=lb,
+                        activity_type=ParcelActivity.ActivityType.WITHDRAWQR
+                    )
                     if pa and isinstance(pa, ParcelActivity):
                         return JsonResponse({"success": True})
                     else:
@@ -118,25 +146,3 @@ def locker_add_activity(request, locker_id: int, activity_type: str):
         return HttpResponseBadRequest()
     except KeyError:
         return HttpResponseBadRequest()
-
-
-def parcel(request, activity_type: int):
-    if request.method == "GET" and "v" in request.GET:
-        try:
-            lb = LockerBase.verify(v_code=request.GET["v"])
-            if "tn" in request.GET:
-                parcel = get_object_or_404(Parcel, tracking_number=request.GET["tn"])
-                if parcel.add_activity(lb, activity_type):
-                    return HttpResponse(f"{parcel.id}")
-                else:
-                    raise Http404
-            else:
-                raise Http404
-        except ObjectDoesNotExist:
-            raise Http404
-
-    elif request.method == "POST" and "v" in request.POST:
-        lb = get_object_or_404(LockerBase, verification_code=request.POST["v"])
-
-    else:
-        raise Http404
